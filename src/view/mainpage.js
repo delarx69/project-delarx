72d0b1653271fb8bf720b04b130cddd59c33d56a
koetee <koetee@proton.me>
2024-08-27 23:52:11 +0300

    refactor: Remove unnecessary import

--- src/view/mainpage.js
+++ src/view/mainpage.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import inputLoginValue from '../login/login'
 
-    const MainPage = () =>{
-
-        
-    return(
+const MainPage = () => {
+    return (
         <div>
             <h1>OAOAOAOAOOAO</h1>
         </div>
     )
-    
-    }
+}
 
 export default MainPage;
